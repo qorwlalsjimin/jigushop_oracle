ba69ee6e77c3725f7d2ad4ad0ab3e834d2290aa0
지민 백 <s2104@e-mirim.hs.kr>
2023-08-07 15:32:35 +0900

    Initial commit

---
+++ src/main/java/kr/jigushop_oracle/jigushop_oracle/JigushopOracleApplication.java
@@ -0,0 +1,13 @@
+package kr.jigushop_oracle.jigushop_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigushopOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JigushopOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/jigushop_oracle/jigushop_oracle/JigushopOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jigushop_oracle.jigushop_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigushopOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
